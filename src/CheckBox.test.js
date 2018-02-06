ea2b3a41c036df620b10fd3e8ac78080aeea3dc4
Bgoldfa <bgoldfa@gap.com>
2018-02-06 10:41:22 -0500

    inital test around check box

---
+++ src/CheckBox.test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+import sinon ,{spy} from 'sinon'
+import CheckBox from '../src/CheckBox'
+
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('The tests for the to do list', () => {
+
+    let wrapper;
+    it('renders without crashing', () => {
+        wrapper = shallow(<CheckBox/>)
+        expect(wrapper.find(List)).to.exist;
+
+    })
+
+
+})
